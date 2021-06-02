d7b97cf7fe1051df679710f9dd996c1e53d3a5ba
liujin <liujin@sunline.cn>
2021-06-02 16:39:20 +0800

    开发header

--- src/components/home_page.vue
+++ src/components/home_page.vue
@@ -7,11 +7,34 @@
       <div class="top">
         <a-row>
           <a-col class="left" :span="16">
-            <img class="iconTitle" src="../assets/icons/lg.png" />
+            <a-icon class="iconTitle" type="trademark" />
             <span class="shuyix">数宜信</span>
             <span class="little">供应链金融一站式服务</span>
           </a-col>
-          <a-col :span="8">col-12</a-col>
+          <a-col class="right" :span="8">
+            <div class="txDiv">
+              <span class="txSpan">
+                <img class="touxiang" src="../assets/image/touxiang.jpg" />
+                <a-icon class="iconDowm" type="down" />
+              </span>
+              <span class="item">
+                <ul @click="clickLi">
+                  <li>
+                    <a-icon type="user" />
+                    <span>我的资料</span>
+                  </li>
+                  <li>
+                    <a-icon type="unlock" />
+                    <span>修改密码</span>
+                  </li>
+                  <li>
+                    <a-icon type="poweroff" />
+                    <span>退出登录</span>
+                  </li>
+                </ul>
+              </span>
+            </div>
+          </a-col>
         </a-row>
       </div>
       <div class="title"></div>
@@ -24,10 +47,25 @@
 export default {
   data() {
     return {
-      background: require("../assets/image/home.jpg"),
+      background: require("../assets/image/3.jpg"),
       backgroundZx: require("../assets/image/zxing.jpg"),
       backgroundZb: require("../assets/image/zhib.jpg"),
       backgroundJc: require("../assets/image/juece.jpg")
+    }
+  },
+  methods: {
+    clickLi(e) {
+      let target = e.target || e.srcElement
+      let name = target.innerText.trim()
+      if (name === "我的资料") {
+        console.log("跳转到我的资料页")
+      }
+      if (name === "修改密码") {
+        console.log("跳转到修改密码页")
+      }
+      if (name === "退出登录") {
+        console.log("执行退出登录逻辑")
+      }
     }
   }
 }
@@ -50,14 +88,13 @@ export default {
   .header {
     width: 100%;
     min-height: 380px;
-    background: gray;
     .top {
       .left {
         padding-left: 500px;
         padding-top: 40px;
         .iconTitle {
-          width: 70px;
-          height: 80px;
+          color: white;
+          font-size: 40px;
         }
         .shuyix {
           font-size: 28px;
@@ -71,6 +108,73 @@ export default {
           border-left: 2px solid white;
           padding-left: 8px;
           font-size: 16px;
+        }
+      }
+      .right {
+        padding-top: 40px;
+        padding-left: 200px;
+        .txDiv {
+          position: relative;
+          .txSpan {
+            display: inline-block;
+            width: 95px;
+            height: 60px;
+            .touxiang {
+              width: 60px;
+              height: 60px;
+              border-radius: 8px;
+            }
+            .iconDowm {
+              color: white;
+              width: 30px;
+              height: 40px;
+              font-size: 15px;
+            }
+            &:hover + .item {
+              visibility: visible;
+            }
+          }
+          .item {
+            visibility: hidden;
+            position: absolute;
+            top: 60px;
+            left: 0;
+            display: inline-block;
+            height: 96px;
+            width: 110px;
+            padding-top: 6px;
+            ul {
+              border-radius: 10px;
+              max-width: 110px;
+              background: #fff;
+              padding: 5px;
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+              height: 90px;
+              overflow: hidden;
+              li {
+                cursor: pointer;
+                line-height: 30px;
+                font-size: 14px;
+                list-style: none;
+                color: gray;
+                width: 110px;
+                padding-left: 14px;
+                &:hover {
+                  color: #fff;
+                  background: #deb887;
+                }
+                span {
+                  margin-left: 3px;
+                }
+              }
+            }
+            &:hover {
+              visibility: visible;
+            }
+          }
         }
       }
     }
